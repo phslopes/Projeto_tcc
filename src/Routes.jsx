d19b8ff4ef48a9019812533f018d7ae1025755cd
Pedro Lopes <pedroppzs01@gmail.com>
2025-06-12 00:12:51 -0300

    feat-evita que acesse outros usuarios digitando o caminho na barra de pesquisa

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; // Importe Navigate
 import LoginPage from "./pages/login";
 import DashboardAdmin from "./pages/Dashboard/admin";
 import Salas from "./pages/salas";
@@ -11,8 +11,7 @@ import StudentDashboard from "./pages/Dashboard/student";
 import TeacherDashboard from "./pages/Dashboard/teacher";
 import AdminLayout from "./components/AdminLayout";
 import TeacherLayout from "./components/TeacherLayout";
-import Associacao from "./pages/associacao";
-
+import PrivateRoute from "./components/PrivateRoute"; 
 
 function RoutesApp() {
   return (
@@ -20,23 +19,33 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
 
-        {/* Layout de Admin: tudo que for /admin/* renderiza dentro do AdminLayout */}
-        <Route path="/admin/*" element={<AdminLayout />}>
-          <Route index element={<DashboardAdmin />} />
-          <Route path="professores" element={<Professores />} />
-          <Route path="disciplinas" element={<Disciplinas />} />
-          <Route path="salas" element={<Salas />} />
-          <Route path="associacao-professor-disciplina" element={<AssociacaoProfessorDisciplinaPage />} /> 
-          <Route path="alocacoes" element={<ProfDiscPage />} />     
+        <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<DashboardAdmin />} />
+            <Route path="professores" element={<Professores />} />
+            <Route path="disciplinas" element={<Disciplinas />} />
+            <Route path="salas" element={<Salas />} />
+            <Route path="associacao-professor-disciplina" element={<AssociacaoProfessorDisciplinaPage />} /> 
+            <Route path="alocacoes" element={<ProfDiscPage />} />     
+            <Route path="*" element={<Navigate to="/admin" replace />} />
+          </Route>
         </Route>
 
-        {/* Rotas de student e teacher não usam esse layout */}
-        <Route path="/aluno" element={<StudentDashboard />} />
-
-        <Route path="/professor/*" element={<TeacherLayout />}>
-          <Route index element={<TeacherDashboard />} />
-          <Route path="reserva" element={<Reserva />} />
+        {/* Rotas de Aluno */}
+        <Route element={<PrivateRoute allowedRoles={['aluno']} />}>
+          <Route path="/aluno" element={<StudentDashboard />} />
         </Route>
+
+        {/* Rotas de Professor*/}
+        <Route element={<PrivateRoute allowedRoles={['professor']} />}>
+          <Route path="/professor" element={<TeacherLayout />}>
+            <Route index element={<TeacherDashboard />} />
+            <Route path="reserva" element={<Reserva />} />
+            <Route path="*" element={<Navigate to="/professor" replace />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
